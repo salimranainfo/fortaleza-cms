64c62bc955422dce447e9a678f6d8dfa06de05b7
Md Salim Rana <rana.salim.info@gmail.com>
2023-09-13 11:02:01 +0600

    Added redis cache plugin

---
+++ config/plugins.js
@@ -0,0 +1,41 @@
+module.exports = {
+  redis: {
+    config: {
+      connections: {
+        default: {
+          connection: {
+            host: "127.0.0.1",
+            port: 6379,
+            db: 0,
+          },
+          settings: {
+            debug: false,
+          },
+        },
+      },
+    },
+  },
+  "rest-cache": {
+    config: {
+      provider: {
+        name: "redis",
+        options: {
+          max: 32767,
+          connection: "default",
+        },
+      },
+      strategy: {
+        enableEtagSupport: true,
+        logs: true,
+        clearRelatedCache: true,
+        maxAge: 3600000,
+        contentTypes: [
+          // list of Content-Types UID to cache
+          "api::contact.contact",
+          "api::homepage.homepage",
+          "api::layout.layout",
+        ],
+      },
+    },
+  },
+};
